6b440f4f30d926e933ecb5c8eee26bdc35c6d6ce
anas zabili <anaszab78@gmail.com>
2021-04-30 19:54:44 +0200

    dessin de la maison au format xml

--- builder/HandlerImpl.java
+++ builder/HandlerImpl.java
@@ -38,17 +38,17 @@ class HandlerImpl extends DefaultHandler implements ContentHandler, ErrorHandler
 
   public void endElement(String uri, String localName, String qName) throws SAXException {
     switch (qName) {
-    case "rectangle":
-      this.builder.make();
-      break;
-    case "line":
-      this.builder.make();
-      break;
-    case "circle":
-      this.builder.make();
-      break;
-    default:
-      break;
+      case "rectangle":
+        this.builder.make();
+        break;
+      case "line":
+        this.builder.make();
+        break;
+      case "circle":
+        this.builder.make();
+        break;
+      default:
+        break;
     }
     int stackSize = this.stack.size() - 1;
 
@@ -61,55 +61,55 @@ class HandlerImpl extends DefaultHandler implements ContentHandler, ErrorHandler
 
   public void startElement(String uri, String localName, String qName, Attributes atts) throws SAXException {
     switch (qName) {
-    case "rectangle":
-      builder.clearAttribut();
-      builder.setContextShape(qName);
+      case "rectangle":
+        builder.clearAttribut();
+        builder.setContextShape(qName);
 
-      builder.setRectangleColor(this.getColorFromString(atts.getValue(0)));
-      break;
-    case "line":
-      builder.clearAttribut();
-      builder.setContextShape(qName);
-      builder.setLineColor(this.getColorFromString(atts.getValue(0)));
+        builder.setRectangleColor(this.getColorFromString(atts.getValue(0)));
+        break;
+      case "line":
+        builder.clearAttribut();
+        builder.setContextShape(qName);
+        builder.setLineColor(this.getColorFromString(atts.getValue(0)));
 
-      break;
-    case "circle":
-      builder.clearAttribut();
-      builder.setContextShape(qName);
-      builder.setCircleColor(this.getColorFromString(atts.getValue(0)));
+        break;
+      case "circle":
+        builder.clearAttribut();
+        builder.setContextShape(qName);
+        builder.setCircleColor(this.getColorFromString(atts.getValue(0)));
 
-      break;
-    case "point":
-      // traitement des point du rectangle
-      if (builder.getContextShape().equals("rectangle") && builder.getRectangleX0() == 0.0) {
-        builder.buildRectangleX0(Integer.parseInt(atts.getValue(0)));
-        builder.buildRectangleY0(Integer.parseInt(atts.getValue(1)));
-      } else if (builder.getContextShape().equals("rectangle") && builder.getRectangleX0() != 0.0) {
-        builder.buildRectangleX1(Integer.parseInt(atts.getValue(0)));
-        builder.buildRectangleY1(Integer.parseInt(atts.getValue(1)));
-      }
-      // traitement des point de la ligne
-      if (builder.getContextShape().equals("line") && builder.getLineX0() == 0.0) {
-        builder.buildLineX0(Integer.parseInt(atts.getValue(0)));
-        builder.buildLineY0(Integer.parseInt(atts.getValue(1)));
-      } else if (builder.getContextShape().equals("line") && builder.getLineX0() != 0.0) {
-        builder.buildLineX1(Integer.parseInt(atts.getValue(0)));
-        builder.buildLineY1(Integer.parseInt(atts.getValue(1)));
-      }
-      // traitement des point du cercle
-      if (builder.getContextShape().equals("circle") && builder.getCircleCX() == 0.0) {
-        builder.buildCircleCX(Integer.parseInt(atts.getValue(0)));
-        builder.buildCircleCY(Integer.parseInt(atts.getValue(1)));
-      }
-      break;
-    case "radius":
-      // if (builder.getContextShape().equals("circle") && builder.getCircleCX() ==
-      // 0.0) {
-      // builder.buildCircleRad(Integer.parseInt(atts.getValue(0)));
-      // }
-      break;
-    default:
-      break;
+        break;
+      case "point":
+        // traitement des point du rectangle
+        if (builder.getContextShape().equals("rectangle") && builder.getRectangleX0() == 0.0) {
+          builder.buildRectangleX0(Integer.parseInt(atts.getValue(0)));
+          builder.buildRectangleY0(Integer.parseInt(atts.getValue(1)));
+        } else if (builder.getContextShape().equals("rectangle") && builder.getRectangleX0() != 0.0) {
+          builder.buildRectangleX1(Integer.parseInt(atts.getValue(0)));
+          builder.buildRectangleY1(Integer.parseInt(atts.getValue(1)));
+        }
+        // traitement des point de la ligne
+        if (builder.getContextShape().equals("line") && builder.getLineX0() == 0.0) {
+          builder.buildLineX0(Integer.parseInt(atts.getValue(0)));
+          builder.buildLineY0(Integer.parseInt(atts.getValue(1)));
+        } else if (builder.getContextShape().equals("line") && builder.getLineX0() != 0.0) {
+          builder.buildLineX1(Integer.parseInt(atts.getValue(0)));
+          builder.buildLineY1(Integer.parseInt(atts.getValue(1)));
+        }
+        // traitement des point du cercle
+        if (builder.getContextShape().equals("circle") && builder.getCircleCX() == 0.0) {
+          builder.buildCircleCX(Integer.parseInt(atts.getValue(0)));
+          builder.buildCircleCY(Integer.parseInt(atts.getValue(1)));
+        }
+        break;
+      case "radius":
+        // if (builder.getContextShape().equals("circle") && builder.getCircleCX() ==
+        // 0.0) {
+        // builder.buildCircleRad(Integer.parseInt(atts.getValue(0)));
+        // }
+        break;
+      default:
+        break;
     }
     stack.add(qName);
   }
@@ -117,14 +117,20 @@ class HandlerImpl extends DefaultHandler implements ContentHandler, ErrorHandler
   // Utile
   public Color getColorFromString(String color) {
     switch (color) {
-    case "black":
-      return Color.BLACK;
-    case "blue":
-      return Color.BLUE;
-    case "red":
-      return Color.RED;
-    default:
-      return Color.BLACK;
+      case "black":
+        return Color.BLACK;
+      case "blue":
+        return Color.BLUE;
+      case "red":
+        return Color.RED;
+      case "yellow":
+        return Color.YELLOW;
+      case "grey":
+        return Color.GRAY;
+      case "green":
+        return Color.GREEN;
+      default:
+        return Color.BLACK;
     }
   }
 }
--- viewer/GraphicViewer.java
+++ viewer/GraphicViewer.java
@@ -8,18 +8,29 @@ import java.util.List;
 
 import javax.swing.*;
 
+import builder.Builder;
+import builder.Director;
+import shapes.AbstractShapeFactory;
+import shapes.NoisyShapeFactory;
+import shapes.PerfectShapeFactory;
 import shapes.Shape;
 import visitor.ConcretVisitorGraphic;
 import visitor.ConcreteVisitorGraphicHorizontalMirror;
 import visitor.Visitor;
 
-public class GraphicViewer extends JFrame implements ActionListener {
+public class GraphicViewer extends JFrame {
 	private final int width = 800;
 	private final int height = 600;
 	private Graphics2D onscreen;
 	private int menuWidth = 100;
 	private JButton horizontaleButton;
+	private JButton drawPencilButton;
+	private JButton drawDrawingButton;
+	private JButton drawHouseButton;
 	private Visitor horizontaleVisitor = new ConcreteVisitorGraphicHorizontalMirror();
+	private Visitor graphicVisitor = new ConcretVisitorGraphic();
+	private AbstractShapeFactory noisyShape = new NoisyShapeFactory();
+	private AbstractShapeFactory perfectShape = new PerfectShapeFactory();
 
 	private List<Shape> shapes;
 
@@ -28,13 +39,6 @@ public class GraphicViewer extends JFrame implements ActionListener {
 		setSize(width + menuWidth, height);
 		setTitle("Afficheur de dessin");
 		shapes = new ArrayList<Shape>();
-
-		Builder builder = new Builder(noisyShape);
-		Director director = new Director(builder);
-		director.construct("./drawing.xml");
-		GraphicViewer gv = new GraphicViewer();
-		List<Shape> demo = builder.getDrawables();
-		gv.draw(demo, graphicVisitor);
 
 		BufferedImage onscreenImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 		ImageIcon icon = new ImageIcon(onscreenImage);
@@ -57,12 +61,48 @@ public class GraphicViewer extends JFrame implements ActionListener {
 
 		});
 
+		// Bouton de dessin du style a partir du fichier xml
+		drawPencilButton = new JButton("dessiner stylo");
+		drawPencilButton.addActionListener(e -> {
+			onscreen.setBackground(Color.WHITE);
+			onscreen.fillRect(0, 0, width, height);
+			this.graphicVisitor.setScreen(onscreen);
+			onscreen.clearRect(0, 0, width, height);
+			drawShapeFromXmlFile("pencil.xml", this.graphicVisitor, this.perfectShape);
+			repaint(100);
+		});
+
+		// Bouton de dessin de la forme à partir du fichier xml
+		drawDrawingButton = new JButton("dessiner forme");
+		drawDrawingButton.addActionListener(e -> {
+			onscreen.setBackground(Color.WHITE);
+			onscreen.fillRect(0, 0, width, height);
+			this.graphicVisitor.setScreen(onscreen);
+			onscreen.clearRect(0, 0, width, height);
+			drawShapeFromXmlFile("drawing.xml", this.graphicVisitor, this.perfectShape);
+			repaint(100);
+		});
+
+		// Bouton de dessin de la maison à partir du fichier xml
+		drawHouseButton = new JButton("dessiner maison");
+		drawHouseButton.addActionListener(e -> {
+			onscreen.setBackground(Color.WHITE);
+			onscreen.fillRect(0, 0, width, height);
+			this.graphicVisitor.setScreen(onscreen);
+			onscreen.clearRect(0, 0, width, height);
+			drawShapeFromXmlFile("house.xml", this.graphicVisitor, this.perfectShape);
+			repaint(100);
+		});
+
 		menu.setBackground(Color.lightGray);
 		menu.setPreferredSize(new Dimension(menuWidth, menuWidth));
 		getContentPane().add(menu, BorderLayout.WEST);
 
-		getContentPane().add(draw, BorderLayout.CENTER);
-		menu.add(horizontaleButton, BorderLayout.CENTER);
+		getContentPane().add(draw);
+		menu.add(horizontaleButton);
+		menu.add(drawPencilButton);
+		menu.add(drawDrawingButton);
+		menu.add(drawHouseButton);
 		onscreen = onscreenImage.createGraphics();
 
 		// Closing any view will quit :
@@ -74,11 +114,6 @@ public class GraphicViewer extends JFrame implements ActionListener {
 	}
 
 	public void draw(List<Shape> shapes, Visitor visitor) {
-		// Shape line = new Line(2);
-		// line.accept(vRed);
-		// line.move();
-		// line.accept(vRed);
-		// line.accept(vBlue);
 		this.shapes = shapes;
 		visitor.setScreen(onscreen);
 
@@ -88,15 +123,14 @@ public class GraphicViewer extends JFrame implements ActionListener {
 		repaint(100);
 	}
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// if (e.getSource() == horizontaleButton) {
-		// System.out.println("wow");
-		// for (Shape shape : this.shapes) {
-		// shape.accept(this.horizontaleVisitor);
-		// }
-		// repaint(100);
-		// }
+	private void drawShapeFromXmlFile(String file, Visitor visitor, AbstractShapeFactory factory) {
+		Builder builder = new Builder(factory);
+		Director director = new Director(builder);
+		director.construct(file);
+		List<Shape> demo = builder.getDrawables();
+		for (Shape shape : demo) {
+			shape.accept(visitor);
+		}
 	}
 
 }
